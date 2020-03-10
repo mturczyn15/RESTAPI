9feb5d08ff40e0d8073f40e6a91b33431d6d5545
mturczyn15 <mturczyn15@gmail.com>
2020-03-10 23:16:45 +0100

    refactor

--- src/main/java/com/crud/task/controller/TrelloController.java
+++ src/main/java/com/crud/task/controller/TrelloController.java
@@ -5,12 +5,11 @@ import com.crud.task.domain.TrelloBoardDto;
 import com.crud.task.domain.TrelloCardDto;
 import com.crud.task.trello.client.TrelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/trello")
 public class TrelloController {
