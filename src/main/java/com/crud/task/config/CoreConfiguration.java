93ed4633c5fb61ecd88346185bd1206a49e14f84
mturczyn15 <mturczyn15@gmail.com>
2020-03-12 00:59:45 +0100

    first commit

--- src/main/java/com/crud/task/config/CoreConfiguration.java
+++ src/main/java/com/crud/task/config/CoreConfiguration.java
@@ -4,11 +4,44 @@ package com.crud.task.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.function.Predicate;
+
+import static com.google.common.base.Predicates.and;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
 @Configuration
-public class CoreConfiguration {
+public class CoreConfiguration  implements WebMvcConfigurer {
+    private static final String NO_ERROR_REGEX = "(?!.*error).*$";
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        // Required by Swagger UI configuration
+        registry.addResourceHandler("/lib/**").addResourceLocations("/lib/").setCachePeriod(0);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(0);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket api() { return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.crud.task.controller"))
+            .paths(PathSelectors.regex("/v1/task/.*"))
+            .build(); }
+
 }
