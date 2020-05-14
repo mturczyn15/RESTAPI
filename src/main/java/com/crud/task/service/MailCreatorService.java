6f1657d6a572ab149878e2c023ec736cd834387b
mturczyn15 <mturczyn15@gmail.com>
2020-05-14 21:34:58 +0200

    task32.2

--- src/main/java/com/crud/task/service/MailCreatorService.java
+++ src/main/java/com/crud/task/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.task.service;
 
 import com.crud.task.config.AdminConfig;
+import com.crud.task.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
     @Autowired
     private AdminConfig adminConfig;
+    @Autowired
+    private CompanyConfig companyConfig;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
@@ -21,6 +24,11 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "https://mturczyn15.github.io");
         context.setVariable("button", "Visit Website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_goal", companyConfig.getCompanyGoal());
+        context.setVariable("company_mail", companyConfig.getCompanyMail());
+        context.setVariable("company_phone", companyConfig.getCompanyPhone());
+        context.setVariable("preview_message", "New task on Trello board!!");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
